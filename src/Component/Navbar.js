e83ba9c616663ca2322e9e74198694f1d59d0772
Pankaj <pankajsarkarweb456@gmail.com>
2023-03-31 13:32:58 +0530

    complelt 1

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import propTypes from "prop-types";
-import { Link } from "react-router-dom";
+// import { Link} from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -9,9 +9,9 @@ export default function Navbar(props) {
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <a className="navbar-brand" to="/">
             {props.titel}
-          </Link>
+          </a>
           <button
             className="navbar-toggler"
             type="button"
@@ -26,14 +26,14 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link " aria-current="page" to="/">
+                <a className="nav-a " aria-current="page" to="/">
                   Home
-                </Link>
+                </a>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <a className="nav-a" to="/about">
                   About
-                </Link>
+                </a>
               </li>
             </ul>
             <form className="d-flex" role="search">
